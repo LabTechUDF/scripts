68df2976704b408a08449c3d6f5852af20cf627a
pedruck <68859909+pedruck@users.noreply.github.com>
2025-04-29 00:20:50 -0300

    correção de bugs e finalização a da main

--- processamento_planilha/main.py
+++ processamento_planilha/main.py
@@ -1,9 +1,17 @@
 from comparando_jsons import ComparisonModule
 import os
 from get_old_collections_from_mongo import ExtractFromMongo
+from upload_collections import UploadCollections
+from normalize import NormalizeDisciplinas
 
-
+print("---------INICIANDO GERAÇÃO DE JSONS INICIAIS---------")
 ComparisonModule.GenerateJsons()
+print("---------INICIANDO GERAÇÃO DE JSONS COMPARADOS---------")
 ComparisonModule.GenerateDeactiveTeachers()
 ComparisonModule.GenerateComparedOffers()
+print("---------INICIANDO NORMALIZAÇÃO E PROCESSAMENTO FINAL DE JSONS---------")
 os.replace("jsonfiles/DISCIPLINAS.json", "comparedjsonfiles/DISCIPLINAS.json")
+
+NormalizeDisciplinas.normalize_disciplinas()
+print("---------INICIANDO UPLOAD PARA A DATABASE---------")
+UploadCollections.upload_collections()
--- processamento_planilha/upload_collections.py
+++ processamento_planilha/upload_collections.py
@@ -7,28 +7,39 @@ from dotenv import load_dotenv
 import json
 from pymongo import MongoClient
 
+class UploadCollections:
 
-load_dotenv()
+    @staticmethod
+    def upload_collections():
+        load_dotenv()
 
-client = MongoClient(os.getenv("MONGO_URI"))
-database = client[os.getenv("MONGO_DATABASE_DESTINATION")]
+        client = MongoClient(os.getenv("MONGO_URI"))
+        database = client[os.getenv("MONGO_DATABASE_DESTINATION")]
 
-collections = {
-    'offers': 'OFERTAS',
-    'periods': 'PERIODOS',
-    'teachers': 'PROFESSORES',
-}
+        collections = {
+            'offers': 'OFERTAS',
+            'teachers': 'PROFESSORES',
+            'disciplines': 'DISCIPLINAS',
+        }
 
 
-for database_collection ,collection_file in collections.items():
-    with open(f'comparedjsonfiles/{collection_file}.json') as json_file:
-        file_data = json.load(json_file)
-        database[database_collection].drop()
+        for database_collection ,collection_file in collections.items():
+            with open(f'comparedjsonfiles/{collection_file}.json', encoding='utf-8') as json_file:
+                file_data = json.load(json_file)
+                database[database_collection].drop()
 
-        if isinstance(file_data, list):
-            database[database_collection].insert_many(file_data)
-        else:
-            print(f"Document '{collection_file}' is an unic data")
-            database[database_collection].insert_one(file_data)
+                if isinstance(file_data, list):
+                    # Handle potential duplicate keys by using replace_one with upsert
+                    for document in file_data:
+                        if '_id' in document:
+                            database[database_collection].replace_one(
+                                {'_id': document['_id']},
+                                document,
+                                upsert=True
+                            )
+                        else:
+                            database[database_collection].insert_one(document)
+                else:
+                    database[database_collection].insert_one(file_data)
 
-        print(f"Document {collection_file} was successfully uploaded!")
+                print(f"Document {collection_file} was successfully uploaded!")
